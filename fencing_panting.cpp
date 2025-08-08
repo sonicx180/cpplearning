148fabb4ae82b11d3b93b082ede8e90bad0ece7f
Sonicx180 <83236320+sonicx180@users.noreply.github.com>
2025-08-08 14:03:42 -0700

    done, switching repos

--- fencing_panting.cpp
+++ fencing_panting.cpp
@@ -1,1 +1,51 @@
 #include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    freopen("paint.in", "r",stdin);
+    freopen("paint.out", "w", stdout);
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+       if ( a == c && b == d) {
+        cout << b - a;
+        return 0;
+    }
+    int greatest_num = d;
+    int smallest_num = a;
+    bool overlap = false;
+    // if a > c && < d then they are overlapping
+    if ( a > c && a < d) {
+        overlap = true;
+    }
+    else if (c > a && c < b) {
+        overlap = true;
+    }
+    else {
+        overlap = false;
+    }
+
+    if(b > greatest_num) {
+        greatest_num = b;
+    }
+
+    if(c < smallest_num) {
+        smallest_num = c;
+    }
+
+
+    if(overlap) {
+        cout << greatest_num - smallest_num;
+    }
+    else {
+        cout << (b-a) + (d-c);
+    }
+
+ 
+}
+
+
--- vectors.cpp
+++ vectors.cpp
@@ -9,6 +9,6 @@ using namespace std;
 int main() {
     vector<int> numbers = {1,2,3,4};
     for (int i = 0; i <  numbers.size(); i++) {
-        cout << numbers[i];
+        
     }
 }
